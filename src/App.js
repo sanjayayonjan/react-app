3ba44fc8e7ed45791ba85416bd6c5c1a154e751b
unknown <sanjaya.yonjan@bidhee.com>
2020-01-02 16:46:25 +0545

    props and state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import './App.css';
-import { Greet } from './components/Greet';
-import Welcome from './components/Welcome';
+// import { Greet } from './components/Greet';
+// import Welcome from './components/Welcome';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
       <h4>Learn Tcaer</h4>
-      <Greet/>
+      {/* <Greet name="wulala">
+        <p>Wulala (Children Props)</p>
+      </Greet>
+      <Greet name="wulala2">
+        <h4>sdfsdfsdf</h4>
+      </Greet>
+      <Greet name="wulala3"/>
+
+
       <Welcome />
+      <Welcome name="wulala">
+        <p>Wulala (Children Props)</p>
+      </Welcome>
+      <Welcome name="wulala2">
+        <h4>sdfsdfsdf</h4>
+      </Welcome>
+      <Welcome name="wulala3"/> */}
+      <Message></Message>
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,6 +4,14 @@ import React from 'react';
 //     return <h4>Hi, Good Morning John Doe.</h4>
 // }
 
-export const Greet = () => <h1>Hi, Good Morning John Doe.</h1>;
+export const Greet = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Hi, Good Morning { props.name }.</h1>
+            {props.children}
+        </div>
+    )
+}
 
 //export default Greet;
